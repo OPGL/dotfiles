6f1372c73a429f4e48857402d69f9f341df68d0a
OPGL <opgl@duck.com>
2024-02-10 15:59:34 +0000

    initial commit

---
+++ .config/nvim/lua/opgl/init.lua
@@ -0,0 +1,4 @@
+require("opgl.lazy")
+require("opgl.lsp_zero")
+require("opgl.telescope")
+require("opgl.lsp_cmp")
---
+++ .config/nvim/lua/opgl/lazy.lua
@@ -0,0 +1,28 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+{'williamboman/mason.nvim'},
+{'williamboman/mason-lspconfig.nvim'},
+{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+{'neovim/nvim-lspconfig'},
+{'hrsh7th/cmp-nvim-lsp'},
+{'hrsh7th/nvim-cmp'},
+{'L3MON4D3/LuaSnip'},
+{"folke/tokyonight.nvim"},
+{"nvim-treesitter/nvim-treesitter"},
+{"nvim-lua/plenary.nvim"},
+{"nvim-telescope/telescope.nvim"}
+}
+
+require("lazy").setup(plugins)
